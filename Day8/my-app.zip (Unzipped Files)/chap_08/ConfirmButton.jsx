e529bc60e36227d4a7f4cad8813563e97982a68b
seonghun.han <hsh931212a@naver.com>
2022-07-28 15:15:02 +0900

    데이터분석 시작

---
+++ Day8/my-app.zip (Unzipped Files)/chap_08/ConfirmButton.jsx
@@ -0,0 +1,17 @@
+import React, {usestate} from "react";
+
+function ConfirmButton(props) {
+    const [isConfirmed, setIsConfirmed] = useState(false);
+
+    const handleConfirm = () => {
+        setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
+    };
+
+    return (
+        <button onClick={handleConfirm} disabled={isConfirmed}>
+            {inConfirmed ? "확인됨" : "확인하기"}
+        </button>
+    );
+}
+
+export default ConfirmButton;
